c3a70d2acc1c29e4b63488086e4704e966ecf469
dathalongbay <dathalongbay@gmail.com>
2019-07-01 19:36:06 +0700

    ``

---
+++ Private/1904e/M6_PHP/005.conditions/index1.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+<pre>
+    Câu lệnh rẽ nhánh
+    if (điều kiện) {
+        các câu lệnh chạy nếu điều kiện là true
+    } else {
+        các câu lệnh chạy nếu điều kiện là false
+    }
+    toán tử so sánh
+    == : so sánh bằng giá trị
+    === : so sánh giá trị + kiểu dữ liệu
+    != : toán tử khác giá trị
+    !== : so sánh khác cả giá trị và kiểu dữ liệu
+    ! : toán tử phủ định
+    > : lớn hơn
+    < : nhỏ hơn
+    && : toán tử and
+    || : toán tử or
+    <> : Toán tử khác
+    ++ : tăng thêm 1 đơn vị
+</pre>
+
+<?php
+$tuoi = 8;
+
+if ($tuoi < 18) {
+    echo "<br> tuổi trẻ em";
+}
+?>
+</body>
+</html>
---
+++ Private/1904e/M6_PHP/005.conditions/index2.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+<?php
+$tuoi = 21;
+if ($tuoi > 18) {
+    echo "<br> Người trưởng thành";
+} else {
+    echo "<br> trẻ em";
+}
+?>
+</body>
+</html>
