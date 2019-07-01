3789c532d0e4a00bc1d92ba4549887fcbf16358a
dathalongbay <dathalongbay@gmail.com>
2019-07-01 18:49:32 +0700

    ``

---
+++ Private/1904e/M6_PHP/003.datatype/index2.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+<pre>
+    Kiểu số nguyên integer
+    Kiểu số thực float
+</pre>
+
+<?php
+$x = 3;
+
+var_dump($x);
+
+$y = 3.5;
+
+echo "<br>";
+var_dump($y);
+
+
+?>
+</body>
+</html>
---
+++ Private/1904e/M6_PHP/003.datatype/index3.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+<pre>
+    Kiểu boolean chứa 2 giá trị : true và false
+</pre>
+
+<?php
+$x = true;
+$y = false;
+var_dump($x);
+echo "<br>";
+var_dump($y);
+?>
+</body>
+</html>
