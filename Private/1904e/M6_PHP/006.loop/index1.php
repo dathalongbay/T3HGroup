8bfa5092068950c45ca517336d25383c787b31b5
dathalongbay <dathalongbay@gmail.com>
2019-07-01 20:45:13 +0700

    ``

---
+++ Private/1904e/M6_PHP/006.loop/index1.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+<pre>
+    BT1 : dùng vòng lặp for in ra các số từ 1 đến 20 nhưng chỉ in số lẻ
+</pre>
+</body>
+</html>
---
+++ Private/1904e/M6_PHP/006.loop/index3.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+</head>
+<body>
+<pre>
+    BT3 : dùng vòng lặp for in ra 1 bảng table gồm có 20 dòng tiêu đề từ 1 đến 20
+</pre>
+
+<div class="container">
+    <div class="row">
+        <table class="table">
+            <thead>
+            <tr>
+                <th>STT</th>
+                <th>Tiêu đề</th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr>
+                <td>1</td>
+                <td>Bản ghi số 1</td>
+            </tr>
+            <tr>
+                <td>2</td>
+                <td>Bản ghi số 2</td>
+            </tr>
+            <tr>
+                <td>3</td>
+                <td>Bản ghi số 3</td>
+            </tr>
+            <tr>
+                <td>4</td>
+                <td>Bản ghi số 4</td>
+            </tr>
+            <tr>
+                <td>5</td>
+                <td>Bản ghi số 5</td>
+            </tr>
+            </tbody>
+        </table>
+    </div>
+</div>
+</body>
+</html>
