8243193a97a95e3704f253e156eeff94dc2c9a4b
dathalongbay <dathalongbay@gmail.com>
2019-07-01 18:43:01 +0700

    ``

---
+++ Private/1904e/M6_PHP/003.datatype/index1.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+<pre>
+    Kiểu chuỗi ( string )
+    Hàm : var_dump() : hàm này được dùng để debug
+    in ra kiểu của biến và giá trị của biến đó
+</pre>
+<?php
+$name = "Nguyễn văn quang";
+echo "<br>" . $name;
+echo '<br>$name';
+echo "<br>$name";
+echo "<br>";
+var_dump($name);
+?>
+</body>
+</html>
