bd4bcae12b348b05f5fb6258b268022cb3153a33
dathalongbay <dathalongbay@gmail.com>
2019-07-01 18:34:21 +0700

    ``

---
+++ Private/1904e/M6_PHP/002.vd2/index4.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+<pre>
+    Hằng số là đối tượng chứa giá trị và không thay đổi trong quá trình chạy
+    khai báo hằng số : define("tên hằng số", "giá trị của hằng số");
+    tên hằng số khác biến
+    biến phải bắt đầu bằng $
+    còn hằng số thì không cần
+    tên hằng số thì nên viết hoa
+</pre>
+
+<?php
+define("NAME", "Nguyễn quang anh");
+echo NAME;
+?>
+</body>
+</html>
