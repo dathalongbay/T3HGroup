185f3d12db120dd7a26e9149f5cb3a28d0e541a5
bacnv <nguyenvanbac33@gmail.com>
2019-07-06 19:25:22 +0700

    1

---
+++ Private/1904e/M6_PHP/007.function/index1.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+<pre>
+    Viết 1 function tính lương nhân viên
+    có 2 tham số truyền vào hàm là : lương tháng và số ngày công
+    sau đó trả về mức lương thực nhận của nhân viên đó
+    Mặc định số ngày công trên 1 tháng là 24 được tính là đủ công
+    ví dụ : lương tháng 10 triệu
+    số ngày công là 12
+    => lương thực lĩnh là 5 triệu
+</pre>
+</body>
+</html>
