755f08c5e773834ed7dfb0e3bb5c09bed8e80853
bacnv <nguyenvanbac33@gmail.com>
2019-07-06 20:03:18 +0700

    1

---
+++ Private/1904e/M6_PHP/007.function/index4.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+<pre>
+     Phạm vi của biến trong PHP
+    https://freetuts.net/xay-dung-ham-trong-php-11.html
+</pre>
+
+
+</body>
+</html>
