20c14ea34752f7091cf683758295ef99aa1d2f53
bacnv <nguyenvanbac33@gmail.com>
2019-07-06 20:00:35 +0700

    1

---
+++ Private/1904e/M6_PHP/007.function/index3.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+<pre>
+    Trong PHP có 2 loại hàm chính :
+    Hàm 1 : các hàm có sẵn trong PHP ví dụ : var_dump() , print_r() ...
+    Hàm 2 : hàm do coder tự định nghĩa
+    chú ý là hàm tự định nghịa không được phép trùng tên hàm có sẵn trong PHP
+    để kiểm tra 1 hàm có sẵn hay chưa sử dụng hàm function_exists()
+    trả về true nếu hàm tồn tại
+    trả về false nếu hàm chưa tồn tại
+</pre>
+</body>
+</html>
