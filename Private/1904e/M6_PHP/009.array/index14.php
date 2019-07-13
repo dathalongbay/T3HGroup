3c20e4a02931458eaf9619830c63af45a2053b09
dathalongbay <dathalongbay@gmail.com>
2019-07-13 19:48:32 +0700

    Default Changelist

---
+++ Private/1904e/M6_PHP/009.array/index14.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+
+<pre>
+    Loop mảng đa chiều
+    Yêu cầu : in được ra tên của các quốc gia và thành phố của nó
+</pre>
+
+<ul>
+    <li>Trung quốc</li>
+    <li>Bắc kinh</li>
+    <li>Thâm quyết</li>
+    <li>Thành đô</li>
+    <li>Hàn quốc</li>
+    <li>Seun</li>
+    ...
+</ul>
+
+<?php
+$array3 = array();
+$array3["china"] = array("name" => "trung quốc", "city" => array("bắc kinh", "thâm quyến", "thành đô"));
+$array3["korea"] = array("name" => "hàn quốc", "city" => array("seun"));
+$array3["japan"] = array("name" => "nhật bản", "city" => array("tokyo", "kyoto"));
+$array3["vn"] = array("name" => "việt nam", "city" => array("hà nội", "hồ chí minh"));
+
+echo "<pre>";
+print_r($array3);
+echo "</pre>";
+
+foreach($array3 as $keyCountry => $country) {
+    foreach ($country as $keyCities => $cities) {
+        if ($keyCities == "city") {
+            foreach ($cities as $keyCity => $city) {
+                echo "<br> " . $city;
+            }
+        }
+    }
+}
+
+
+?>
+</body>
+</html>
