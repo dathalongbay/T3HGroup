79512479a992c586f91a1cbc1e66c29c903cd688
dathalongbay <dathalongbay@gmail.com>
2019-07-13 18:59:53 +0700

    Default Changelist

--- Private/1904e/M6_PHP/009.array/index12.php
+++ Private/1904e/M6_PHP/009.array/index12.php
@@ -1,7 +1,38 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: T3H
- * Date: 7/13/2019
- * Time: 6:28 PM
- */
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+
+    <pre>
+        Các hàm xử lý mảng trong PHP
+    </pre>
+
+    <?php
+    $array1 = array(0,1,2);
+
+    /**
+     * Đếm số phần tử của mảng
+     */
+    echo "<br> hàm count : " . count($array1);
+    /**
+     * Kiểm tra 1 biến có phải 1 mảng hay không
+     */
+    echo "<nr> hàm is_array ";
+    var_dump(is_array($array1));
+
+    /**
+     * Kiểm tra xem 1 mảng có rỗng không
+     * empty($array1) trả về true nếu mảng rỗng và trả về false nếu mảng không rỗng
+     * !empty($array1) trả về true nếu mảng có dữ liệu và trả về false nếu mảng không có dữ liệu
+     */
+    echo "<br>";
+    var_dump(empty($array1));
+
+
+    ?>
+
+</body>
+</html>
