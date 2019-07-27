cb176465820e314bad665e665e5f03421044317a
Trung Doan <dathalongbay@gmail.com>
2019-07-27 20:05:18 +0700

    Default Changelist

---
+++ Private/1904e/M6_PHP/014.includevarequire/001/index.php
@@ -0,0 +1,11 @@
+<?php
+
+
+$cv = tinhchuvihinhtron(3);
+
+
+/**
+ * gặp thông báo lỗi
+ * Fatal error: Uncaught Error: Call to undefined function tinhchuvihinhtron() in C:\xampp\htdocs\1901ephp1\day7\napinclude\index.php:4 Stack trace: #0 {main} thrown in C:\xampp\htdocs\1901ephp1\day7\napinclude\index.php on line 4
+ */
+echo "Chu vi : " , $cv;
---
+++ Private/1904e/M6_PHP/014.includevarequire/002/index.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * Câu lệnh include "đường dẫn của file";
+ * thì có thể sử dụng những hàm hoặc class hoặc biến hoặc hằng số của file các bạn vừa nạp
+ */
+include "functions.php";
+
+$cv = tinhchuvihinhtron(3);
+
+echo "Chu vi : " , $cv;
