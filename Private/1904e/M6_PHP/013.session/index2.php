4f8a794a4ec03cfdf800410665565186a03b6b56
Trung Doan <dathalongbay@gmail.com>
2019-07-27 19:23:29 +0700

    Default Changelist

---
+++ Private/1904e/M6_PHP/013.session/index2.php
@@ -0,0 +1,15 @@
+<?php
+// bắt buộc phải khởi session
+session_start();
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+
+<h1>Khởi động session</h1>
+</body>
+</html>
