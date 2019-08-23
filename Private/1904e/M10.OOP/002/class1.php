d218cffdba1f386c3f0c0b888c001a0d5e1459eb
dathalongbay <dathalongbay@gmail.com>
2019-08-23 20:45:48 +0700

    Default Changelist

---
+++ Private/1904e/M10.OOP/002/class1.php
@@ -0,0 +1,43 @@
+<?php
+class Vuon {
+
+    public $cannang = 50;
+
+    public $chieucao = 150;
+
+    public $tuoi = 50;
+
+    public function adn() {
+        echo "<br>" . __METHOD__;
+    }
+
+}
+
+
+class Nguoi
+{
+
+    public $hochieu;
+
+    public $sodienthoai;
+
+    public $email;
+
+    public function didulich() {
+        echo "<br>" . __METHOD__;
+    }
+
+
+    public function demokethua() {
+        echo "<br>" . __METHOD__;
+        echo "<br> " . $this->cannang;
+        echo "<br> " . $this->chieucao;
+        echo "<br> " . $this->tuoi;
+    }
+
+
+}
+
+$an = new Nguoi();
+$an->demokethua();
+
