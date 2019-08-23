580f8c3d53de742fec480660f9ccb4a40393d515
dathalongbay <dathalongbay@gmail.com>
2019-08-23 19:41:06 +0700

    Default Changelist

---
+++ Private/1904e/M10.OOP/001/class4.php
@@ -0,0 +1,53 @@
+<?php
+
+// khai báo class
+class Student {
+
+
+    /**
+     * khai báo các thuộc tính
+     * của class
+     * thuộc tính chỉ tính chất đặc điểm
+     * của class đó
+     */
+
+    public $name = "nguyen quang tuan";
+
+    public $age = 21;
+
+    public $location = "ha noi";
+
+    public $point = 7;
+
+
+    /**
+     * Phương thức
+     */
+
+    public function printInfo() {
+
+        echo "<br>" . __METHOD__;
+        echo "<br>tên : " . $this->name;
+        echo "<br>tuổi : " . $this->age;
+        echo "<br>địa điểm : " . $this->location;
+        echo "<br>điểm tổng kết : " . $this->point;
+
+    }
+
+}
+
+// khởi tạo đối tượng từ class
+$tuan = new Student();
+
+/**
+ * muốn in ra cấu trúc của 1 đối tượng
+ */
+echo "<pre>";
+print_r($tuan);
+echo "</pre>";
+
+// gọi đến 1 method trong class
+$tuan->printInfo();
+
+
+
