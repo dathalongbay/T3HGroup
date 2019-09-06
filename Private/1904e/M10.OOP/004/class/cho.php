267453eed947e80c5099694843b7fb9563388abb
dathalongbay <dathalongbay@gmail.com>
2019-09-06 18:57:24 +0700

    Default Changelist

---
+++ Private/1904e/M10.OOP/004/class/cho.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Admin
+ * Date: 06/09/2019
+ * Time: 6:53 PM
+ */
+include_once "../abstract/dongvat.php";
+
+
+class Cho extends Dongvat {
+
+
+
+}
---
+++ Private/1904e/M10.OOP/004/class/ga.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Admin
+ * Date: 06/09/2019
+ * Time: 6:54 PM
+ */
+include_once "../abstract/dongvat.php";
+
+class Ga extends Dongvat {
+
+}
